02a6f673975217bac9ae54ed69716484bfbb7611
Simon Katz <simon@l3mon4.de>
2025-09-08 21:17:46 +0200

    initial version of transcoding fuse-filesystem.

---
+++ dirmap/opus_fs.py
@@ -0,0 +1,60 @@
+from dirmap import MappedFSTree, FileMap, map_fname
+import subprocess, os, re, sys
+from slugify import slugify
+
+def extmap(ext: str) :
+    if ext != "flac" :
+        return ext
+    return "opus"
+
+def namemap(name: str) :
+    # leave 6 characters for the extension, should suffice :)
+    # (max of 256 chars on some old filesystems.)
+    return slugify(
+        name,
+        replacements=[
+            ['Ü', 'UE'],
+            ['ü', 'ue'],
+            ['Ä', 'AE'],
+            ['ä', 'ae'],
+            ['Ö', 'OE'],
+            ['ö', 'oe'],
+        ],
+        max_length=250)
+
+def datamap(path) :
+    if path[-4:] == "flac" :
+        proc = subprocess.Popen(
+            ["ffmpeg", "-i", path, "-b:a", "128k", "-map_metadata", "0", "-f", "opus", "pipe:1"],
+            stdin=None, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
+        return proc.stdout
+    elif path[-3:] == "cue" :
+        with open(path, "r") as f:
+            text = f.read()
+            FNAME_GROUPIDX=1
+            matches_b2f = list(re.finditer(r"FILE\s+\"([^\"]+)\"", text))
+            matches_b2f.reverse()
+            # iterate matches back to front so we don't have to adjust
+            # string-indices.
+            for match in matches_b2f :
+                targetfile = match.group(FNAME_GROUPIDX)
+                # out map-fname can't handle path-separators.
+                assert(targetfile.find("/") == -1)
+
+                mapped_fname = map_fname(targetfile, name_map = namemap, ext_map = extmap)
+                text = text[0:match.start(FNAME_GROUPIDX)] + mapped_fname + text[match.end(FNAME_GROUPIDX):]
+            
+            return text.encode("utf-8")
+    else :
+        return open(path, "rb")
+
+def sizemap(de: os.DirEntry) :
+    # safe upper bound for files converted from flac to opus, and also for
+    # unconverted files.
+    return de.stat(follow_symlinks=True).st_size
+
+def main() :
+    filemap = FileMap(extension_map = extmap, data_map = datamap)
+
+    fstree = MappedFSTree(sys.argv[1], file_map = filemap, name_map = namemap)
+    fstree.init_fuse(sys.argv[2])
---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "Empty nix flake.";
+
+  inputs = {
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {self, nixpkgs-unstable, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs-unstable { inherit system; };
+        pylibs = pypkgs: with pypkgs; [ fuse python-slugify ];
+      in {
+        packages.default = pkgs.python3Packages.buildPythonApplication {
+          pname = "dirmap";
+          version = "0.1";
+          pyproject = true;
+          src = ./.;
+          build-system =  with pkgs.python3Packages; [hatchling];
+          # can't find fuse in runtime-check.. But it works, so disable it until
+          # I want to look into this.
+          dontCheckRuntimeDeps = true;
+          dependencies = pylibs pkgs.python3Packages;
+        };
+        devShell = pkgs.mkShell {
+          packages = [
+            (pkgs.python3.withPackages pylibs)
+            pkgs.bashInteractive
+          ];
+        };
+      }
+    );
+}
